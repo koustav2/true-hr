df8fb169438637ff56741d3b5406108ffda177b7
Koustav_Quantta <kaustav.maity@breatheagain.life>
2026-06-19 19:37:22 +0530

    State-based holidays + HR Leave Configuration admin UI

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_truehr_app_TrueHrApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.truehr.app.TrueHrApp"
-)
-public final class _com_truehr_app_TrueHrApp {
-}
